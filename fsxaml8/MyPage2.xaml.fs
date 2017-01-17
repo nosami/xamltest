bf60a054afb94420cae0616f8bb94713df5531a3
nosami <jasonimison@gmail.com>
2017-01-17 17:33:55 +0000

    stash

--- fsxaml8/MyPage2.xaml.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace fsxaml8
-
-open Xamarin.Forms
-open Xamarin.Forms.Xaml
-
-type fsxaml3xPage() =
-    inherit ContentPage()
-    let _ = base.LoadFromXaml(typeof<fsxaml3xPage>)
--- fsxaml8/fsxaml8Page.xaml.fs
+++ fsxaml8/fsxaml8Page.xaml.fs
@@ -2,11 +2,8 @@
 
 open Xamarin.Forms
 open Xamarin.Forms.Xaml
-type xaml = Xamarin.Forms.XamlProviders.XamlProvider<"fsxaml8Page">
+type xaml = XamlProviders.XamlProvider<"fsxaml8Page">
 type fsxaml8Page() =
-    inherit ContentPage()
-    //let xaml = XamlProviders.XamlTypeProvider()
-    do xaml.myLabel.Text <- "Huh!?"
-    //let _ = base.LoadFromXaml(typeof<fsxaml8Page>)
-    //let label = base.FindByName<Label>("myLabel")
-    //do label.Text <- "Huh!?"
+    inherit xaml() // xaml needs to inherit ContentPage
+    do 
+        base.myLabel.Text <- "Huh!?"
